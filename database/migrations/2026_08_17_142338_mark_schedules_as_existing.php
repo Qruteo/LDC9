a2b3ef8134293b41d70ae1f1b27d87e4b8996d98
Qruteo <captaindharma85@gmail.com>
2026-08-19 18:18:42 +0700

    Fix: Migrate bug

--- database/migrations/2026_08_17_142338_mark_schedules_as_existing.php
+++ database/migrations/2026_08_17_142338_mark_schedules_as_existing.php
@@ -8,7 +8,17 @@ return new class extends Migration
 {
     public function up(): void
     {
-        //
+        Schema::create('schedules', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('schedule_id')
+              ->constrained('schedules')
+              ->cascadeOnDelete();
+
+        $table->string('schedule_code')->unique();
+
+        $table->timestamps();
+    });
     }
 
     public function down(): void
